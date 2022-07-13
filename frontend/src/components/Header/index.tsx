8ba8e8cede2ad2b32bf0f2a193fbcbb49e8af838
PauloFH99 <paulohenriquefs1998@gmail.com>
2022-07-13 19:18:15 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import icon from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={icon} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://github.com/PauloFH99">Paulo Henrique Fernandes dos Santos</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
