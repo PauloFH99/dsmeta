cc854d4439f707f191d510b003f8f070cd0c981b
PauloFH99 <paulohenriquefs1998@gmail.com>
2022-07-15 23:29:02 -0300

    msg formact

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,22 @@
-import SalesCard from "./components/SalesCard"
-import Header from "./components/Header"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+
 function App() {
-  return(
+  return (
     <>
-    <Header />
-    <main>
-      <section id="sales">
-        <div className="dsmeta-container">
-          <SalesCard />
-        </div>
-      </section>
-    </main>  
+      <ToastContainer />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,19 +1,21 @@
-import logo from '../../assets/img/logo.svg'
-import './styles.css'
+import icon from '../../assets/img/logo.svg';
+import './styles.css';
 
 function Header() {
     return (
-        <header>
-            <div className="dsmeta-logo-container">
-                <img src={logo} alt="DSMeta" />
-                <h1>DSMeta</h1>
-                <p>
-                    Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
-                </p>
-            </div>
-        </header>
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={icon} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por
+                        <a href="https://github.com/PauloFH99">Paulo Henrique Fernandes dos Santos</a>
+                    </p>
+                </div>
+            </header>
+        </>
     )
 }
 
-export default Header
+export default Header;
